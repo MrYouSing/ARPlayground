6195d131e0bd02d697a0bf97407d63213c108636
YouSing <yousing@sina.cn>
2020-12-01 17:14:44 +0800

    1) Added the ScreenUV prototype;

---
+++ Assets/Common/Scripts/GetBackground.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+namespace YouSingStudio.AR {
+	public class GetBackground:MonoBehaviour {
+		#region Fields
+
+		public ARCameraManager arManager;
+		public ARCameraBackground arBackground;
+		public Camera cameraBackground;
+		public ARFoundationBackgroundRenderer rendererBackground;
+		public Camera cameraFinal;
+		public RenderTexture renderTexture;
+		public Material material;
+		public Material[] materials;
+
+		public RawImage rawImage;
+
+		#endregion Fields
+
+		#region Unity Messages
+
+		protected virtual void Awake() {
+			if(arManager==null) {
+				arManager=GetComponent<ARCameraManager>();
+			}
+			if(arBackground==null) {
+				arBackground=FindObjectOfType<ARCameraBackground>();
+			}
+			if(cameraFinal==null) {
+			if(arManager!=null) {
+				cameraFinal=arManager.GetComponent<Camera>();
+			}}
+		}
+
+		protected virtual void OnEnable() {
+			arManager.frameReceived+=OnCameraFrameReceived;
+		}
+
+		protected virtual void OnDisable() {
+			arManager.frameReceived-=OnCameraFrameReceived;
+		}
+
+
+		protected unsafe virtual void OnCameraFrameReceived(ARCameraFrameEventArgs eventArgs) {
+			if(true) {
+				int w=Screen.width,h=Screen.height;
+				if(renderTexture==null||renderTexture.width!=w||renderTexture.height!=h) {
+					if(renderTexture!=null) {RenderTexture.ReleaseTemporary(renderTexture);}
+					renderTexture=RenderTexture.GetTemporary(w,h,0,RenderTextureFormat.ARGB32);
+				}
+				cameraBackground.targetTexture=renderTexture;
+				if(eventArgs.projectionMatrix.HasValue) {
+					cameraBackground.projectionMatrix=eventArgs.projectionMatrix.Value;
+				}
+				if(rendererBackground==null) {
+					rendererBackground=new ARFoundationBackgroundRenderer();
+					rendererBackground.mode=ARRenderMode.MaterialAsBackground;
+					rendererBackground.backgroundMaterial=arBackground.material;
+					rendererBackground.camera=cameraBackground;
+				}
+				//
+				Texture texture=renderTexture;
+				if(true) {
+					Material mat;
+					for(int i=0,imax=materials?.Length??0;i<imax;++i) {
+						mat=materials[i];
+						if(mat!=null) {
+							mat.mainTexture=texture;
+						}
+					}
+					//
+					if(rawImage!=null) {
+						rawImage.texture=texture;
+					}
+				}
+			}
+		}
+
+		#endregion Unity Messages
+	}
+}
---
+++ Assets/ScreenUV/Scripts/MeshModifier.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+
+namespace YouSingStudio.Rendering {
+	public class MeshModifier
+		:MonoBehaviour
+	{
+		#region Fields
+
+		public Transform root;
+		public Mesh mesh;
+		public MeshFilter meshFilter;
+		public SkinnedMeshRenderer skinnedMeshRenderer;
+
+		[System.NonSerialized]protected bool m_IsStartCalled;
+		[System.NonSerialized]protected Mesh m_Mesh;
+
+		#endregion Fields
+
+		#region Unity Messages
+
+		protected virtual void Awake() {
+			if(meshFilter==null) {meshFilter=GetComponent<MeshFilter>();}
+			if(skinnedMeshRenderer==null) {skinnedMeshRenderer=GetComponent<SkinnedMeshRenderer>();}
+		}
+
+		protected virtual void Start() {
+			//
+			m_IsStartCalled=true;
+			ModifyMesh();
+		}
+
+		protected virtual void OnEnable() { 
+			if(m_IsStartCalled) {
+				ModifyMesh();
+			}
+		}
+
+		protected virtual void Update() {
+			ModifyMesh();
+		}
+
+		#endregion Unity Messages
+
+		#region Methods
+
+		protected virtual void InitMesh() {
+			if(m_Mesh!=null) {
+				return;
+			}
+			//
+			if(root==null) {root=transform;}
+			Mesh tmp=mesh;
+				m_Mesh=GetMesh();
+				if(m_Mesh!=null) { 
+					m_Mesh=Mesh.Instantiate(m_Mesh);
+					SetMesh(m_Mesh);
+				}
+			mesh=tmp;
+		}
+
+		public virtual Mesh GetMesh() {
+			if(skinnedMeshRenderer!=null) {return skinnedMeshRenderer.sharedMesh;}
+			if(meshFilter!=null) {return meshFilter.sharedMesh;}
+			return mesh;
+		}
+
+		public virtual void SetMesh(Mesh value) {
+			if(skinnedMeshRenderer!=null) {skinnedMeshRenderer.sharedMesh=value;}
+			if(meshFilter!=null) {meshFilter.sharedMesh=value;}
+			mesh=value;
+		}
+
+		public virtual void ModifyMesh() {
+		}
+
+		#endregion Methods
+	}
+}
